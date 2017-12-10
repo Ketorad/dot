f520969b7fef71b06ecb2a02bebb2759918971db
Eivind <ketorad@gmail.com>
2017-12-10 22:49:24 +0100

    added special character note

---
+++ i3/scripts/tab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $1 == "reverse" ]]; then
+#	i3-msg focus right
+#	i3-msg focus down
+#	i3-msg focus left
+#	i3-msg move right
+#	i3-msg focus up
+#	i3-msg move left
+#	i3-msg focus right
+#	i3-msg focus up
+#	i3-msg focus left
+#	i3-msg resize grow width 30 px or 30 ppt
+notify-send "herro"
+else
+	i3-msg move right
+	i3-msg focus down
+	i3-msg move left
+	i3-msg resize grow width 30 px or 30 ppt
+fi
